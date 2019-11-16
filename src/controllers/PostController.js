8d85797030488d93f44d2e93d047093398140693
Wallace Frota <frotadev@gmail.com>
2019-11-16 16:30:52 -0300

    atualizado arquivo que controla postagens

--- src/controllers/PostController.js
+++ src/controllers/PostController.js
@@ -1,3 +1,5 @@
+// DEVELOPED BY: WALLACE FROTA - @frotadev
+
 // importando model de post
 const Post = require('../models/Post');
 
@@ -8,5 +10,35 @@ module.exports = {
         const postagens = await Post.findAll();
 
         return res.json(postagens)
+    },
+    async show(req, res) {
+        // exibir uma postagem específica pelo id
+        const postagens = await Post.findOne({where: {id: req.params.id}});
+
+        return res.json(postagens);
+    },
+    async postapp(req, res) {
+        // criando postagens
+        const postagens = await Post.create(req.body);
+
+        return res.json(postagens);
+    },
+    async update(req, res) {
+        // update dos dados via id
+        await Post.update({
+            title: req.body.title,
+            description: req.body.description,
+            url: req.body.url},
+
+            {where: {id: req.params.id}} // atualizando pelo id condição where
+        )
+
+        return res.send()
+    },
+    async delete(req, res) {
+        // deletar postagem pelo id
+        await Post.destroy({where: {id: req.params.id}});
+
+        return res.send();
     }
 }
