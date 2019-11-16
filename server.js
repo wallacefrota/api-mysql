33712eb3ed69fe113f488015cc41e3164ff610e3
Wallace Frota <frotadev@gmail.com>
2019-11-16 11:59:37 -0300

    atualizado arquivo principal permitindo envio de dados via json

--- server.js
+++ server.js
@@ -5,7 +5,11 @@ const requireDir = require('require-dir');
 // app recebendo toda a função express
 const app = express();
 
-requireDir('./src/models')
+// requerendo todo o diretório de models
+requireDir('./src/models');
+
+// permitindo envio de json na aplicação
+app.use(express.json())
 
 // acesso a aplicação via porta 8081
 app.listen(8081, (req, res) => {
