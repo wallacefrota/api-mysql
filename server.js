ed86db5a6b4565f61f3851b6a907b0634510bc76
Wallace Frota <frotadev@gmail.com>
2019-11-16 11:49:59 -0300

    atualizado arquivo principal para requerer os módulos usando require-dir

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 // importando pacotes/modulos
 const express = require('express');
+const requireDir = require('require-dir');
+
 // app recebendo toda a função express
 const app = express();
+
+requireDir('./src/models')
 
 // acesso a aplicação via porta 8081
 app.listen(8081, (req, res) => {
