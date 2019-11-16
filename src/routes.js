1e7843e31e6435bcf9b8842bac1f0f904074436f
Wallace Frota <frotadev@gmail.com>
2019-11-16 16:31:57 -0300

    atualizado arquivo routes.js para trabalhar com qualquer tipo de requisição na rota

--- src/routes.js
+++ src/routes.js
@@ -1,3 +1,5 @@
+// DEVELOPED BY: WALLACE FROTA - @frotadev
+
 // importando modulos
 const express = require('express');
 const routes = express.Router();
@@ -6,6 +8,10 @@ const routes = express.Router();
 const PostController = require('./controllers/PostController');
 // rotas
 routes.get('/postagens', PostController.index);
+routes.get('/postagens/:id', PostController.show);
+routes.post('/postagens', PostController.postapp);
+routes.put('/postagens/:id', PostController.update);
+routes.delete('/postagens/:id', PostController.delete);
 
 // exportando routes
 module.exports = routes;
